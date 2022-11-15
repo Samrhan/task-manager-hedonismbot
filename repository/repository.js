1a48dafee9bdab4c92632e7f2ceb12512beafb5d
Samuel Bader <samuel.bader@carbon-it.com>
2022-11-15 10:11:17 +0100

    refacto(repository): should get all tasks

--- repository/repository.js
+++ repository/repository.js
@@ -1,4 +1,16 @@
 class Repository {
+    todos;
+    constructor() {
+        this.todos = new Map();
+    }
+
+    save(task) {
+        this.todos.set(task.id, task);
+    }
+
+    getAll() {
+        return [...this.todos.values()];
+    }
 }
 
 module.exports = {Repository};
